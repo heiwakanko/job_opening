720854aee074215987acd78b3083972eed14bd8b
heiwakanko <heiwakanko@users.noreply.github.com>
2023-07-15 07:15:54 +0000

    deploy: a578cb28e7f6b090b8040c1432def36d6f8f4ce8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "798b737644642e1f2501b0509579b152",
-"/": "798b737644642e1f2501b0509579b152",
+const RESOURCES = {"index.html": "9fc4061e71b42500ae2df90fd4f406d0",
+"/": "9fc4061e71b42500ae2df90fd4f406d0",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "a36550d885997bbe52041d91ad177332",
 "main.dart.js": "3bdc816cc742867e263cb09614424e27",
